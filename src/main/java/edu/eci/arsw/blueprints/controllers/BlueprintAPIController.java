6bde1e5459ec8b76bc1925696668d10a08e127ee
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2024-09-26 09:54:31 -0500

    agregue el autowired en la clase controller

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -7,7 +7,11 @@ package edu.eci.arsw.blueprints.controllers;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+
+
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
-    
-    
-    
-    
-    
+
+    @Autowired
+    BlueprintsServices bps;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetRecursoXX() {
+        try {
+            //obtener datos que se enviarán a través del
+
+            bps.getAllBlueprints();
+            return null; //new ResponseEntity<>(data,HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            //Logger.getLogger(XXController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error bla bla bla", HttpStatus.NOT_FOUND);
+        }
+    }
 }
+
 
